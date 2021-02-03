cf6e4ee075eedb23ea1f8af182180d998efa1139
Sanchit Gangwar <sanchitgangwar@gmail.com>
2021-02-03 20:28:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+In the `script.sh` file, change `SOURCE_PATH` to the path where images are present.
+
+Run `./script.sh`
---
+++ script.sh
@@ -0,0 +1,7 @@
+SOURCE_PATH="./Images"
+SNIPPETS_OUTPUT="./snippets.tex"
+
+touch $SNIPPETS_OUTPUT
+for f in "${SOURCE_PATH}/*.jpg"; do
+    printf '\includepdf[width=\\textwidth,height=\\textheight,keepaspectratio]{%s}\n' $f >> $SNIPPETS_OUTPUT
+done
