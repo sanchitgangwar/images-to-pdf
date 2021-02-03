9b24c69f1af49a58dd1920a82dc388085d493c22
Sanchit Gangwar <sanchitgangwar@gmail.com>
2021-02-03 20:37:09 +0530

    changes to script

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 SOURCE_PATH="./Images"
 SNIPPETS_OUTPUT="./snippets.tex"
 
-touch $SNIPPETS_OUTPUT
+echo '' > $SNIPPETS_OUTPUT
 for f in "${SOURCE_PATH}/*.jpg"; do
     printf '\includepdf[width=\\textwidth,height=\\textheight,keepaspectratio]{%s}\n' $f >> $SNIPPETS_OUTPUT
 done
+
+pdflatex output.tex
